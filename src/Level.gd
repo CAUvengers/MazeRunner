6cbda16b195e9fdbc33741de3bb737d5ed48afdd
Junseok Lee <shawn950303@gmail.com>
2020-11-26 15:24:44 +0900

    Add Victory Scene

--- src/Level.gd
+++ src/Level.gd
@@ -3,6 +3,7 @@ extends Spatial
 onready var monster = $GridMap/Monster
 onready var player = $GridMap/player
 onready var orb_container = $GridMap/OrbContainer
+onready var fader = $Fader
 
 var collected_orbs = 0
 var total_orb_count = 0
@@ -13,8 +14,14 @@ func _ready():
 	total_orb_count = orb_container.get_child_count()
 	
 	player.connect("orb_collected", self, "on_orb_collected")
+	fader.connect("fade_finished", self, "on_fade_finished")
 	
 func on_orb_collected():
 	collected_orbs += 1
 	if collected_orbs >= total_orb_count:
-		get_tree().change_scene("res://src/menu_components/MainMenu.tscn")
+		fader.set_playback_speed(0.5)
+		fader.fade_out()
+		
+func on_fade_finished():
+	get_tree().change_scene("res://src/VictoryScene.tscn")
+	
